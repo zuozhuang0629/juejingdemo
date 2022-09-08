ba46ebbfab593661caf58a2b3cd81993cd9f90eb
zuoz <zuoz@163.com>
2022-09-08 18:08:30 +0800

    添加部分代码

--- app/src/main/java/com/jiejin/image/MainActivity.kt
+++ app/src/main/java/com/jiejin/image/MainActivity.kt
@@ -3,13 +3,26 @@ package com.jiejin.image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jiejin.image.ui.theme.ImageviewTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ImageUI()
                 }
             }
         }
@@ -30,14 +43,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ImageUI() {
+    Image(
+        painterResource(R.drawable.ic_temp),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier =  Modifier
+            .width(100.dp)
+            .height(100.dp)
+            .clip(shape = RoundedCornerShape(50))
+            .border(
+                width = 2.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(50)
+            )
+
+    )
+
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Newline greeting")
 @Composable
 fun DefaultPreview() {
     ImageviewTheme {
-        Greeting("Android")
+        ImageUI()
     }
 }
